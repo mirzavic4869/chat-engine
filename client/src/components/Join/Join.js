279e7897fed7564b25ae377c329ce0717a4052c3
mirzavic4869 <avicenamirzam@gmail.com>
2022-10-05 13:01:13 +0700

    create infobar page

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import "../Join/Join.css";
+import "./Join.css";
 
 const Join = () => {
 	const [name, setName] = useState("");
